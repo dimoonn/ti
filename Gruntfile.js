f91583178f67725037451b298647b8abee16b059
Damian Kozyra <damiankozyra@Damians-MBP-2.home>
2016-04-24 20:03:18 +0200

    ti

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        concat: {
+            bundle: {
+                src: [
+                    'src/js/**/*.js'
+                ],
+                dest: 'web/assets/main.js'
+            },
+            sass: {
+                src: [
+                    'src/css/grid.css'
+                ],
+                dest: 'web/assets/main.css'
+            }
+        },
+        sass: {
+            options: {
+                sourceMap: false,
+                outputStyle: 'compressed'
+            },
+            dist: {
+                files: {
+                    'web/assets/main.css': 'web/assets/main.scss'
+                }
+            }
+        },
+        
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.registerTask('default', ['concat', 'sass']);
+};
